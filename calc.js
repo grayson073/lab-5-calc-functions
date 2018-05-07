b32b4345160ac7e579d038eb00fd5f7f994eb8a3
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-07 14:28:56 -0700

    Fixed testing console logs via calc.js

--- calc.js
+++ calc.js
@@ -5,7 +5,7 @@ function remainder(x, y) {
     var remainder = x % y;
 
     return {
-        value: '= ' + remainder,
+        value: remainder,
         description: 'The remainder of ' + x + ' divided by ' + y + ' is ' + remainder + '.'
     };
 }
@@ -14,7 +14,7 @@ function add(x, y) {
     var add = Number(x) + Number(y);
 
     return {
-        value: '= ' + add,
+        value: add,
         description: 'The sum of ' + x + ' + ' + y + ' is ' + add + '.'
     };
 }
@@ -23,7 +23,7 @@ function subtract(x, y) {
     var subtract = x - y;
 
     return {
-        value: '= ' + subtract,
+        value: subtract,
         description: 'The answer to ' + x + ' - ' + y + ' is ' + subtract + '.'
     };
 }
@@ -32,7 +32,7 @@ function divide(x, y) {
     var divide = x / y;
     
     return {
-        value: '= ' + divide.toFixed(2), //toFixed(2) rounds decimal to 2 places e.g. 1.67
+        value: divide, //toFixed(2) rounds decimal to 2 places e.g. 1.67
         description: 'The number ' + x + ' divided by ' + y + ' is ' + divide.toFixed(2) + '. (Rounded to 2 decimal places)'
     };
 }
