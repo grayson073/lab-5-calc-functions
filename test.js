abf16daf79bc4dd9425d68feb1936b9d2fb41af4
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-07 11:01:15 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//AC #3 function return remainder
+if(dividend % divisor === result) {
+    console.log('TDD-AC3 - function returns correct result - SUCCESS');
+} else {
+    console.error('TDD-AC3 - function returns correct result - FAIL');
+}
